24c502c4c1af3365c0f757eeadb1aea163d062c8
KuldeepSahoo917 <kuldeepsahoo917@gmail.com>
2026-09-05 08:09:45 +0000

    Update frontend product API calls

--- frontend/src/redux/Actions/ProductActions.js
+++ frontend/src/redux/Actions/ProductActions.js
@@ -17,7 +17,7 @@ export const listProduct = (keyword = " ", pageNumber = " ") => async (dispatch)
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST });
         const { data } = await axios.get(
-            `/api/products?keyword=${keyword}&pageNumber=${pageNumber}`
+            `/products?keyword=${keyword}&pageNumber=${pageNumber}`
         );
         dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
     } catch (error) {
@@ -36,7 +36,7 @@ export const listProduct = (keyword = " ", pageNumber = " ") => async (dispatch)
 export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST });
-        const { data } = await axios.get(`/api/products/${id}`);
+        const { data } = await axios.get(`/products/${id}`);
         dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
     } catch (error) {
         dispatch({
@@ -66,7 +66,7 @@ export const createProductReview = (productId, review) => async (dispatch, getSt
             },
         };
 
-        await axios.post(`/api/products/${productId}/review`, review, config);
+        await axios.post(`/products/${productId}/review`, review, config);
         dispatch({ type: PRODUCT_CREATE_REVIEW_SUCCESS });
     } catch (error) {
         const message =
